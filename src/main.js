08d3abf38b5a5d95b5f7b1cbe0a34e7b6efae06c
Shylin Denis <tawnship406@gmail.com>
2024-11-20 13:09:03 +0200

    Strat

---
+++ src/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Accordion("#faqAccordion", {
+    duration: 400,
+    showMultiple: false,
+    openOnInit: [0],
+    elementClass: "ac",
+    triggerClass: "ac-trigger",
+    panelClass: "ac-panel",
+    activeClass: "is-active",
+  });
+});
